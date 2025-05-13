179534e17e97ef903193009fe069b11b4e15cb59
UditKamdar <63956159+UditKamdar@users.noreply.github.com>
2025-05-13 00:55:19 -0700

    Time: 313 ms (44.01%), Space: 44.9 MB (84.64%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,62 @@
+class Solution {
+    class TrieNode{
+        boolean isEndOfWord = false;
+        TrieNode[] children = new TrieNode[26];
+    }
+
+    TrieNode root = new TrieNode();
+    
+    int[][] directions = {{0,-1},{-1,0},{0,1},{1,0}};
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> ans = new ArrayList<>();
+        boolean[][] visited;
+        for(String word: words){
+            addWordIntoTrie(word);
+        }
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                visited = new boolean[board.length][board[0].length];
+                dfs(board,i,j,ans, new StringBuilder(),visited, root);
+            }
+        }  
+        return ans; 
+    }
+
+    public void addWordIntoTrie(String word){
+        TrieNode node = root;
+        for(char c : word.toCharArray()){
+            if(node.children[c-'a'] == null){
+                node.children[c-'a'] = new TrieNode();
+            }
+            node = node.children[c-'a'];
+        }
+        node.isEndOfWord = true;
+    }
+
+
+    public void dfs(char[][] board,int row, int column, List<String> ans, StringBuilder sb, boolean[][] visited, TrieNode node){
+        if(row < 0 || row == board.length || column < 0 || column == board[0].length) return;
+        if(visited[row][column]) return;
+
+        char ch = board[row][column];
+        if(node.children[ch-'a'] == null) return;
+
+        visited[row][column] = true;
+        sb.append(ch);
+        node = node.children[ch - 'a'];
+
+        if(node.isEndOfWord){
+            ans.add(sb.toString());
+            node.isEndOfWord = false;
+        }
+
+        for(int[] dir : directions){
+            dfs(board, row + dir[0], column + dir[1], ans, sb, visited, node);
+        }
+
+        visited[row][column] = false;
+        sb.setLength(sb.length() - 1);
+    }
+}
