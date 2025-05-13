c914d4c8e21025632af73a6f3542a138c2686945
UditKamdar <63956159+UditKamdar@users.noreply.github.com>
2025-05-13 00:58:16 -0700

    Time: 308 ms (45.04%), Space: 44.5 MB (98.84%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -10,14 +10,13 @@ class Solution {
 
     public List<String> findWords(char[][] board, String[] words) {
         List<String> ans = new ArrayList<>();
-        boolean[][] visited;
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for(String word: words){
             addWordIntoTrie(word);
         }
 
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                visited = new boolean[board.length][board[0].length];
+            for(int j = 0; j < board[i].length; j++){  
                 dfs(board,i,j,ans, new StringBuilder(),visited, root);
             }
         }  
