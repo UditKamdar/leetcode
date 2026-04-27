8c74417be24fa685d6dac448416ecc7d78da4f3b
UditKamdar <63956159+UditKamdar@users.noreply.github.com>
2026-04-27 19:52:35 -0400

    Time: 0 ms (100%), Space: 44.4 MB (81.39%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -3,25 +3,27 @@ class Solution {
         
         List<String> res = new ArrayList<>();
 
-        dfs(0,0,"",n,res);
+        dfs(0,0,new StringBuilder(),n,res);
 
         return res;
     }
 
 
-    void dfs(int openP, int closeP, String s, int n, List<String> res){
+    void dfs(int openP, int closeP, StringBuilder s, int n, List<String> res){
 
         if(openP == closeP && (openP + closeP) == (n * 2)){
-            res.add(s);
+            res.add(s.toString());
             return;
         }
 
         if(openP < n){
-            dfs(openP + 1, closeP , s + "(", n, res);
+            dfs(openP + 1, closeP , s.append('('), n, res);
+            s.deleteCharAt(s.length() - 1);
         }
 
         if(closeP < openP){
-            dfs(openP, closeP + 1, s + ")", n, res);
+            dfs(openP, closeP + 1, s.append(')'), n, res);
+            s.deleteCharAt(s.length() - 1);
         }
     }
 }
